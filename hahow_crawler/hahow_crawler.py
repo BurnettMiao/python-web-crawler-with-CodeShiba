5eee185b1899899c0299894cecdfd2e5fcfd79e5
Burnett Miao <searchfor1987@gmail.com>
2024-04-29 17:49:16 +0800

    hahow 爬蟲練習-finish

--- hahow_crawler/hahow_crawler.py
+++ hahow_crawler/hahow_crawler.py
@@ -1,6 +1,28 @@
 import requests
+import pandas as pd
 
 url = 'https://api.hahow.in/api/products/search?category=COURSE&filter=PUBLISHED&limit=24&page=0&sort=TRENDING'
 headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 }
+
+response = requests.get(url, headers=headers)
+if response.status_code == 200:
+  data = response.json()
+  # print(data['data']['courseData']['products'])
+  products = data['data']['courseData']['products']
+  course_list = []
+  for product in products:
+    # print(product['title'])
+    course_data = [
+      product['title'],
+      product['averageRating'],
+      product['price'],
+      product['numSoldTickets']
+    ]
+    course_list.append(course_data)
+  df = pd.DataFrame(course_list, columns=['課程名稱', '評價', '價格', '購買人數'])
+  df.to_excel('courses.xlsx', index=False, engine='openpyxl')
+  print('Save!')
+else:
+  print('無法取得網頁')
