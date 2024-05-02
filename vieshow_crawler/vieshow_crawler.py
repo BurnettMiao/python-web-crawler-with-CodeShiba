9b99b4275e14d1a632ce9b33749c249fb51e5f95
Burnett Miao <searchfor1987@gmail.com>
2024-05-02 17:17:33 +0800

    爬完再爬下一頁功能尚未完成

--- vieshow_crawler/vieshow_crawler.py
+++ vieshow_crawler/vieshow_crawler.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
 
 data_list = []
 
@@ -18,9 +19,23 @@ def fetch_data(url):
     data_list.append([name, engName,date])
   print(data_list)
 
+  allPageList = soup.find('section', class_='pagebar')
+  allPageList = allPageList.find_all('a')
+  maxNum = len(allPageList) - 1
+  minNum = len(allPageList) - (len(allPageList) - 1)
+  for index in range(minNum, maxNum):
+    print(index)
+    newUrl = f'{url}?p=index'
+    time.sleep(5)
+    if index < maxNum:
+      fetch_data(newUrl)
+
+  # time.sleep(2)
+  # print(minIndex, maxIndex)
+  
 url = 'https://www.vscinemas.com.tw/vsweb/film/index.aspx'
 
 fetch_data(url)
 
-df = pd.DataFrame(data_list, columns=['中文電影名稱', '英文電影名稱', '上映日期'])
-df.to_excel('現正熱映.xlsx', index=False, engine='openpyxl')
+# df = pd.DataFrame(data_list, columns=['中文電影名稱', '英文電影名稱', '上映日期'])
+# df.to_excel('現正熱映.xlsx', index=False, engine='openpyxl')
