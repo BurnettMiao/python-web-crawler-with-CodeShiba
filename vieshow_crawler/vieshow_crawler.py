aefeb0f3f373fc4ea3deca3907695d70a8121b7f
Burnett Miao <searchfor1987@gmail.com>
2024-05-02 14:21:57 +0800

    威秀影城爬取完成

--- vieshow_crawler/vieshow_crawler.py
+++ vieshow_crawler/vieshow_crawler.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+data_list = []
 
 def fetch_data(url):
   response = requests.get(url)
@@ -7,10 +10,17 @@ def fetch_data(url):
   movies = soup.find_all('section', class_='infoArea')
   for movie in movies:
     name = movie.find('a')
-    print(name.text)
-  
-
+    name = name.text.strip() # 使用 .strip() 去除空格
+    engName = movie.find('h3')
+    engName = engName.text.strip()
+    date = movie.find('time')
+    date = date.text.strip()
+    data_list.append([name, engName,date])
+  print(data_list)
 
 url = 'https://www.vscinemas.com.tw/vsweb/film/index.aspx'
 
 fetch_data(url)
+
+df = pd.DataFrame(data_list, columns=['中文電影名稱', '英文電影名稱', '上映日期'])
+df.to_excel('現正熱映.xlsx', index=False, engine='openpyxl')
